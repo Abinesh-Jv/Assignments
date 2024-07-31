87e094ed41c803fa4441461f9ade010feba131c1
Abinesh-Jv <103888564+Abinesh-Jv@users.noreply.github.com>
2024-07-31 10:33:10 +0530

    Update Main.java

--- shapes/Main.java
+++ shapes/Main.java
@@ -1,1 +1,21 @@
 
+public class Main {
+    public static void main(String[] args) {
+        Shape genericShape = new Shape("No Color");
+        Circle circle = new Circle("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+
+        System.out.println("Generic Shape Area: " + genericShape.calculateArea());
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+
+        System.out.println();
+
+        genericShape.displayInfo();
+        circle.displayInfo();
+        rectangle.displayInfo();
+        System.out.println();
+        System.out.println("Overloaded Method for Rectangle Area: " + genericShape.calculateArea(4, 6));
+        System.out.println("Overloaded Method for Circle Area: " + genericShape.calculateArea(5));
+    }
+}
