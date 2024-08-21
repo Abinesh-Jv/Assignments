fab0dc9a332d88eaa4f7784f12f94670d15b9bc4
Abinesh-Jv <103888564+Abinesh-Jv@users.noreply.github.com>
2024-08-21 09:57:17 +0530

    Add files via upload

---
+++ MethodOverloading/MainPerson.java
@@ -0,0 +1,12 @@
+public class MainPerson {
+public static void main(String[] args) {
+    Person p1 = new Person("Abinesh");
+    Person p2 = new Person("Abinesh",17);
+    Person p3 = new Person("Abinesh",17,"Chennai");
+
+
+    p1.display();
+    p2.display();
+    p3.display();
+}
+}
